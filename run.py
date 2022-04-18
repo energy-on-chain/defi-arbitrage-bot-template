dbe8323f08f164c5ba24fefdae18bf1c8e1b94dc
hartimat <matthew.t.hartigan@gmail.com>
2022-04-18 08:33:14 -0500

    Spooky and spirit swap routers now successfully connected via http. Next step calc price

--- run.py
+++ run.py
@@ -7,22 +7,73 @@
 # the EOC defi arbitrage bot. 
 ###############################################################################
 import datetime
+import utils
+import json
+from config import config_params
+from exchanges import spooky
 
 
 # AUTHENTICATE 
-if config_params['in_production']:
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="credentials.json"
+# if config_params['in_production']:
+    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="credentials.json"
 
 
 # FUNCTIONS
+def check_spookyswap_to_spiritswap(web3_connection, factory, router, token0_symbol, token0_address, token1_symbol, token1_address, pair_address, pair_abi):
+    """ FIXME: function description goes here """
+    queried_pair_address = factory.functions.getPair(token0_address, token1_address).call()    # get the contract address for the desired exchange pair
+
+    if queried_pair_address == pair_address:
+        print('Token pair contract addresses match!')
+        pair_contract = web3_connection.eth.contract(address=pair_address, abi=pair_abi)    # instantiate the exchange pair contract
+        # pair_swap_filter = pair_contract.
+        max_token1_for_token0 = router.functions.getAmountOut().call()
+    else:
+        print('Addresses do not match!')
+        exit(0)
+    
+    print(pair_contract.all_functions())
+    print(router.all_functions())
+    # print(factory.all_functions())
+
+
 def run():
+    """ FIXME: function description goes here """
+
+    # HTTP Connect to Node
     print(config_params['name'] + ' ' + config_params['version'] + ' is busy printing money... [' + str(datetime.datetime.utcnow()) + ']')
-    print('Beginning execution... [' + str(datetime.datetime.utcnow()) + ']')
-    print('Get DEX #1 quote... [' + str(datetime.datetime.utcnow()) + ']')
-    print('Get DEX #2 quote... [' + str(datetime.datetime.utcnow()) + ']')
-    print('Make comparison... [' + str(datetime.datetime.utcnow()) + ']')
-    print('Execute trade... [' + str(datetime.datetime.utcnow()) + ']')
-    print('Output results... [' + str(datetime.datetime.utcnow()) + ']')
+    print('Connecting to blockchain... [' + str(datetime.datetime.utcnow()) + ']')    
+    web3_connection = utils.get_http_provider_connection(config_params['ftm_http'], config_params['ankr_username'], config_params['ankr_password'])    
+    # web3_connection = utils.get_wss_provider_connection(config_params['ftm_wss'])    # wss connect to a web3 provider
+
+    # Connect to DEX Routers
+    print('Loading contracts... [' + str(datetime.datetime.utcnow()) + ']')    # create contract objects
+    spookyswap_factory_contract = web3_connection.eth.contract(address=config_params['spookyswap_factory_contract'], abi=json.loads(config_params['spookyswap_factory_abi']))    # create spookyswap exchange contract
+    spookyswap_router_contract = web3_connection.eth.contract(address=config_params['spookyswap_router_contract'], abi=json.loads(config_params['spookyswap_router_abi']))    # create spookyswap exchange contract
+    spiritswap_factory_contract = web3_connection.eth.contract(address=config_params['spiritswap_factory_contract'], abi=json.loads(config_params['spiritswap_factory_abi']))    # create spiritswap exchange contract
+    spiritswap_router_contract = web3_connection.eth.contract(address=config_params['spiritswap_router_contract'], abi=json.loads(config_params['spiritswap_router_abi']))    # create spiritswap exchange contract
+    print('Spoookyswap available functions:')
+    print(spookyswap_router_contract.all_functions())
+    print('Spiritswap available functions:')
+    print(spiritswap_router_contract.all_functions())
+    print()
+
+    # Connect to Targeted DEX Pair Contracts
+    # FIXME
+
+    # Check for Arbitrage
+    print('Checking for arbitrage... [' + str(datetime.datetime.utcnow()) + ']')    # look for arbitrage
+    try:
+        print('Spookyswap...')
+        # check_spookyswap_to_spiritswap(web3_connection, spookyswap_factory_contract, spookyswap_router_contract, 'WFTM', config_params['wftm_contract'], 'USDC', config_params['usdc_contract'], config_params['wftm_usdc_spookyswap_pair_contract'], config_params['wftm_usdc_spookyswap_pair_abi'])
+        
+        print('Spiritswap...')
+    except Exception as e:
+        print('FIXME: There was an error during the check for arbitrage process!')
+        print(e)
+
+    print('Executing trades... [' + str(datetime.datetime.utcnow()) + ']')
+    print('Outputting results... [' + str(datetime.datetime.utcnow()) + ']')
 
 
 # ENTRY POINT
@@ -35,10 +86,19 @@ else:
     run()
 
 
+# COMPLETED:
+# Setup framework for defi bot (DONE)
+# Setup config file with appropriate contracts (DONE)
+# Setup node connection to FTM blockchain (DONE)
+# Connect to and query spookyswap factory and router (DONE)
+# Connect to and query spiritswap factory and router (DONE)
+
+
 # TODO:
-# Implement spooky.py (test txn)
-# Implement spirit.py (test txn)
-# Implement arbitrage
-# Decide whether to persist exchange connections or redo each tim
-# Email notifications
-# Emergency shutdown swtich
+# convert to wss feed(?)
+# figure out how to calculate price on univ2
+# make first auto transaction from spooky and spirit
+# setup arbitrage logic
+# setup results output (gcfs csv, dash)
+# deploy to vm
+# Consider putting it in a farm in the intermediate time between swaps
---
+++ utils.py
@@ -0,0 +1,37 @@
+###############################################################################
+# PROJECT: EOC Defi Arbitrage Bot Template
+# AUTHOR: Matt Hartigan
+# DATE: 15-April-2022
+# FILENAME: utils.py
+# DESCRIPTION: Utility functions for the defi bot.
+###############################################################################
+import requests
+import base64
+import websocket
+from web3 import Web3
+
+
+# PROVIDER CONNECTIONS
+def get_http_provider_connection(http, username, password):
+    session = requests.Session()
+    encoded_login = username + ':' + password
+    encoded_login = encoded_login.encode('ascii')
+    b64 = base64.b64encode(encoded_login).decode('ascii')
+    session.headers.update({'Authorization': 'Basic ' + b64})
+    w3 = Web3(Web3.HTTPProvider(http, session=session))
+    if w3.isConnected():
+        print('Web3 Connection to FTM node successfully established!')
+    else:
+        print('Error establishing Web3 Connection!')
+    return w3
+
+
+def get_wss_provider_connection(wss):
+    w3 = Web3(Web3.WebsocketProvider(wss))
+    return w3
+
+
+def get_account_balance(web3_connection, address):
+    balance = web3_connection.eth.get_balance(address)
+    print(balance)
+    return(balance)
